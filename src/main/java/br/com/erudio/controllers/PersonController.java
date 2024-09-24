de2610aa0585c95b76bde3b33f7c5caea60ca7c3
Jheorgenes Warlley <jheorgenes@gmail.com>
2024-09-24 15:29:42 -0300

    -- Aprendendo a adicionar CORS origin no projeto spring boot -- Adicionando CORS via Controller ou CORS geral, liberando por origens específicas. -- Adicionando Teste de Integração para PersonController, testando as rotas com CORS.

--- src/main/java/br/com/erudio/controllers/PersonController.java
+++ src/main/java/br/com/erudio/controllers/PersonController.java
@@ -23,6 +23,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+//@CrossOrigin
 @RestController
 @RequestMapping("/api/person/v1")
 @Tag(name = "People", description = "Endpoints for Managing People")
@@ -32,6 +33,7 @@ public class PersonController {
 	private PersonServices service;
 	
 	
+//	@CrossOrigin(origins = "http://localhost:8080")
 	@GetMapping(
 			value = "/{id}", 
 			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML }) //Usando o produces e consumes para interpretação do swagger futuramente
@@ -71,6 +73,7 @@ public class PersonController {
 		return service.findAll();
 	}
 	
+//	@CrossOrigin(origins = {"http://localhost:8080", "https://erudio.com.br"})
 	@PostMapping(
 			consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML }, 
 			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
